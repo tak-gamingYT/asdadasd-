32e5de009ea8c3d6dc351d6b2f67a53c536e2aa3
tak-gamingYT <83076510+tak-gamingYT@users.noreply.github.com>
2023-11-15 16:28:05 +0700

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php
+//EN - FR
+//CONFIGURATION - CONFIG
+//SITE NAME - Nom du site
+$sitename = 'Layer7 Dstat';
+// CAPS ONLY - Majuscule seulement
+
+//Bit or byte - Bit ou octet
+// US=bit and/et EU= octet
+$zone = 'US';
+
+//Data transferred or received  -  Donnée transférée ou recus
+//RX = received/recus TX = transferred/transféerée
+$datatype = 'RX';
+
+//Language - Langue
+//FR = francais/french - EN = english/anglais
+$lang = 'EN';
+
+//Name of interface - Nom de l'interface
+//Usually : 'eth0' - Generalement :'eth0'
+$interface = 'eth0';
+
+// Ip of your server
+$ip = "127.0.0.1";
+$port = "80";
+
+//Graph title
+$Layer4Title = "Layer 4 ==>{$ip} PORT {$port} <==";
+$Layer7Title = "Layer 7 ==>{$ip} PORT {$port} <==";
